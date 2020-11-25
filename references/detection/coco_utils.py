7746a9ef2d0b190529b8ff4afb9c3c363b178319
Hansal Shah <s.hansal@yahoo.com>
2020-11-25 13:43:12 -0500

    Fix eval issue in coco_utils.py

--- references/detection/coco_utils.py
+++ references/detection/coco_utils.py
@@ -199,8 +199,8 @@ def get_coco_api_from_dataset(dataset):
     for _ in range(10):
         if isinstance(dataset, torchvision.datasets.CocoDetection):
             break
-        if isinstance(dataset, torch.utils.data.Subset):
-            dataset = dataset.dataset
+#         if isinstance(dataset, torch.utils.data.Subset):
+#             dataset = dataset.dataset
     if isinstance(dataset, torchvision.datasets.CocoDetection):
         return dataset.coco
     return convert_to_coco_api(dataset)
